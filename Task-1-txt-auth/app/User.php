d5f11ca18e337e5f77d3e85e18bd3a441d893fba
Empty-stu <Zasxcdfv7673>
2020-07-29 00:32:39 +0300

    User entity been added.

--- Task-1-txt-auth/app/User.php
+++ Task-1-txt-auth/app/User.php
@@ -1,39 +1,27 @@
 <?php
 
+
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Illuminate\Validation\UnauthorizedException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
+class User
 {
-    use Notifiable;
+    public $name;
+    private $password;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    public function __construct($username, $password)
+    {
+        $this->name = $username;
+        $this->password = $password;
+    }
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function checkPasswordIdentity($incomingPassword)
+    {
+        if($this->password !== $incomingPassword) {
+            throw new UnauthorizedException();
+        }
+    }
 }
